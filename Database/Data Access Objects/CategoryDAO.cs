d74e5a8d2aee46295eec355658cd7a3d9b772973
Ondřej Kulhavý <okulhav@gmail.com>
2023-05-20 22:26:44 +0200

    feat: add category dao

--- Database/Data Access Objects/CategoryDAO.cs
+++ Database/Data Access Objects/CategoryDAO.cs
@@ -4,30 +4,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BookWiseApp.Database.Models;
+using System.Data.SqlClient;
+
 
 
 namespace BookWiseApp.Database.Data_Access_Objects
 {
     public class CategoryDAO : IDAO<Category>
     {
+        private SqlConnection connection;
+        public CategoryDAO()
+        {
+            this.connection = DbConnection.connection;
+            if (connection == null) throw new Exception("Conection to database failed in CategoryDAO constructor");
+        }
         public void Delete(Category element)
         {
-            throw new NotImplementedException();
+            string query = "DELETE FROM Category WHERE CategoryID = @CategoryID";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@CategoryID", element.Id);
+            command.ExecuteNonQuery();
         }
 
         public IEnumerable<Category> GetAll()
         {
-            throw new NotImplementedException();
+            string query = "SELECT * FROM Category";
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Category category = new Category(
+                    Convert.ToInt32(reader[0].ToString()),
+                    reader[1].ToString(),
+                    reader[2].ToString()
+                );
+                yield return category;
+            }
+            reader.Close();
         }
 
         public Category? GetByID(int id)
         {
-            throw new NotImplementedException();
+            Category? category = null;
+            string query = "SELECT * FROM Category WHERE CategoryID = @CategoryID";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@CategoryID", id);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                category = new Category(
+                    Convert.ToInt32(reader[0].ToString()),
+                    reader[1].ToString(),
+                    reader[2].ToString()
+                );
+            }
+            reader.Close();
+            return category;
         }
 
-        public void Save(Category element)
+        public void Save(Category category)
         {
-            throw new NotImplementedException();
+            string query = "INSERT INTO Category (CategoryName) VALUES (@CategoryName)";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@CategoryName", category.Name);
+            command.ExecuteNonQuery();
         }
 
     }
