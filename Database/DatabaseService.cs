6fcf5a4a1fb880ae5b88ff48799c9db55140e7bc
Ondřej Kulhavý <okulhav@gmail.com>
2023-05-20 20:05:39 +0200

    feat:

--- Database/DatabaseService.cs
+++ Database/DatabaseService.cs
@@ -1,10 +1,39 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BookWiseApp.Database.Models
 {
-    public class DatabaseService { }
+    public class DatabaseService
+    {
+        private static SqlConnection connection;
+
+        public DatabaseService()
+        {
+            InitializeConnection();
+        }
+
+        public void InitializeConnection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings[
+                "Database"
+            ].ConnectionString;
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            if (connection != null)
+            {
+                connection.Close();
+                connection.Dispose();
+                connection = null;
+            }
+        }
+    }
 }
